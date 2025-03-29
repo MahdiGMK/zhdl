ef18188b795b4944d4c75bb07fc23f3638f63c1e
MahdiGMK <mbahramiand@gmail.com>
2025-03-29 21:40:33 +0330

    api in zig

--- build.zig
+++ build.zig
@@ -18,31 +18,30 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
 
     const lib_unit_tests = b.addTest(.{ .root_module = lib_mod });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 
-    const sample_mod = b.createModule(.{
-        .root_source_file = b.path("samples/sample1.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    sample_mod.addImport("zhdl", lib_mod);
-    const sample = b.addExecutable(.{
-        .name = "sample1",
-        .root_module = sample_mod,
-    });
-    b.installArtifact(sample);
-    const run_cmd = b.addRunArtifact(sample);
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-    const run_step = b.step("run-sample1", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-    const exe_unit_tests = b.addTest(.{ .root_module = sample_mod });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    // const sample_mod = b.createModule(.{
+    //     .root_source_file = b.path("samples/sample1.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // sample_mod.addImport("zhdl", lib_mod);
+    // const sample = b.addExecutable(.{
+    //     .name = "sample1",
+    //     .root_module = sample_mod,
+    // });
+    // b.installArtifact(sample);
+    // const run_cmd = b.addRunArtifact(sample);
+    // run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+    // const run_step = b.step("run-sample1", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+    // const exe_unit_tests = b.addTest(.{ .root_module = sample_mod });
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    // test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,123 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub const Delay = struct {
+    val_in_ps: usize,
+    pub fn micro(val: usize) Delay {
+        return .{ .val_in_ps = val * 1000_000 };
+    }
+    pub fn nano(val: usize) Delay {
+        return Delay{ .val_in_ps = val * 1000 };
+    }
+    pub fn pico(val: usize) Delay {
+        return .{ .val_in_ps = val };
+    }
+    pub fn toPico(self: *Delay) usize {
+        return self.val_in_ps;
+    }
+    pub fn toNano(self: *Delay) usize {
+        return (self.val_in_ps + 999) / 1000;
+    }
+    pub fn toMicro(self: *Delay) usize {
+        return (self.val_in_ps + 999_999) / 1000_000;
+    }
+};
+pub const ClkTrigger = enum(u2) { none = 0, posedge = 1, negedge = 2, both = 3 };
+pub const AssignType = enum(u1) { blocking, nonBlocking };
+pub const AssignDetail = struct { t: AssignType = .blocking, d: Delay = .fromPico(0) };
+pub const SynthTarget = enum { verilog, vhdl, ngspice };
+pub fn CTX(comptime underlayingType: type) type {
+    _ = underlayingType;
+    const T = struct {
+        pub fn init() @This() {
+            @compileError("TODO : not implemented");
+        }
+        pub fn assign(self: *@This(), onto: *WritableWire, value: *Wire, details: AssignDetail) void {
+            _ = self;
+            _ = onto;
+            _ = value;
+            _ = details;
+            @compileError("TODO : not implemented");
+        }
+        pub fn delay(self: *@This(), amt: Delay) void {
+            _ = self;
+            _ = amt;
+            @compileError("TODO : not implemented");
+        }
+        pub fn clk(self: *@This(), trigger: ClkTrigger) void {
+            _ = self;
+            _ = trigger;
+            @compileError("TODO : not implemented");
+        }
+
+        pub fn iter(self: *@This(), comptime count: usize) []@This() {
+            _ = self;
+            _ = count;
+            @compileError("TODO : not implemented");
+        }
+        pub fn subcircuit(self: *@This()) @This() {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+        pub fn submodule(self: *@This(), comptime SubCtx: type) SubCtx {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+
+        /// start
+        pub fn start(self: *@This()) void {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+        /// compute wires
+        pub fn step(self: *@This()) void {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+        /// step, then toggle clk
+        pub fn stepClk(self: *@This()) void {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+
+        /// synthesis
+        pub fn synth(self: *@This(), target: SynthTarget) void {
+            _ = self;
+            _ = target;
+            @compileError("TODO : not implemented");
+        }
+        // compute max delay
+        pub fn dmax(self: *@This()) Delay {
+            _ = self;
+            @compileError("TODO : not implemented");
+        }
+    };
+    _ = T;
+    @compileError("TODO : not implemented");
+    // return T;
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+fn Wire(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
+}
+fn WritableWire(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
+}
+pub fn Reg(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
+}
+pub fn Input(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
+}
+pub fn Output(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
+}
+pub fn Inout(comptime underlayingType: type) type {
+    _ = underlayingType;
+    @compileError("TODO : not implemented");
 }
